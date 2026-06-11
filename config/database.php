16e6554754ca8c57c30e45af2dd6126db961d12a
Gustavodevinfo <gustavo.anjospc@gmail.com>
2026-06-11 15:29:05 -0300

    Teste de roteamento

--- config/database.php
+++ config/database.php
@@ -1,15 +1,22 @@
 <?php
+
 $host = 'localhost';
 $dbname = 'atendelab';
 $user = 'root';
 $password = '';
+
 try {
- $pdo = new PDO(
- "mysql:host=$host;port=3307;dbname=$banco;charset=utf8",
- $user,
- $password
- );
- $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $pdo = new PDO(
+        "mysql:host=$host;port=3307;dbname=$dbname;charset=utf8",
+        $user,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (PDOException $e) {
- die('Erro ao conectar com o banco de dados: ' . $e->getMessage());
+
+    die('Erro ao conectar com o banco de dados: ' . $e->getMessage());
+
 }
